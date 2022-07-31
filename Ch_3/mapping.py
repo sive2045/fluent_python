89c90a36e14207112271e848f5c51f23a29ad3e4
sive2045 <lc9902130509@gmail.com>
2022-07-31 16:34:42 +0900

    fix: ch.3

--- Ch_3/mapping.py
+++ Ch_3/mapping.py
@@ -12,3 +12,14 @@ hash(tl) # unhashable type: 'list'
 
 tf = (1, 2, frozenset([30, 40]))
 hash(tf)
+
+# dict Comprehensions
+DIAL_CODES = [
+    (86, 'China'),
+    (91, 'India'),
+    (1, 'United States'),
+    (55, 'Brazil'),
+    (100, 'Korea')
+]
+
+country_code = {country:code for code, country in DIAL_CODES}
